13b9c0454a8972726b901f4da2b401b0e1796f72
NormanKalyuzhny <innerspiritu@gmail.com>
2024-07-01 20:48:30 +0300

    Added another test

--- integrations/tests/noAuth/practice-general-components.test.js
+++ integrations/tests/noAuth/practice-general-components.test.js
@@ -1,8 +1,12 @@
 import { test,expect } from "@playwright/test"  
+import { before, beforeEach } from "node:test"
 
 test.describe('Basic actions', ()=>{
-    test('Buttons', async ({page})=>{
+    test.beforeEach('', async ({page})=>{
         await page.goto('https://commitquality.com/practice-general-components')
+    })
+    test('Buttons', async ({page})=>{
+        
         const btnContainer = page.locator('//div[@class="button-container"]')
         const clickMeBtn = btnContainer.locator('//button[@data-testid="basic-click"]')
         const doubleClickBtn = btnContainer.locator('//button[@data-testid="double-click"]')
@@ -20,5 +24,35 @@ test.describe('Basic actions', ()=>{
             await rightClickBtn.click( { button: "right" })
             expect(await btnContainer.innerText()).toContain('Button right mouse clicked')
         })
+    })
+    test('Radio buttons', async ({page}) => {
+        const compContainer = page.locator('//div[@class="radio-buttons-container container-outline"]')
+        const radioBtn1 = compContainer.locator('//input[@data-testid="option1"]')
+        const radioBtn2 = compContainer.locator('//input[@data-testid="option2"]')
+
+        await test.step('Radio button 1', async() => {
+            await radioBtn1.click()  
+            expect(await compContainer.innerText()).toContain('option1', 'clicked')
+        })
+        await test.step('Radio button 2', async() => {
+            await radioBtn2.click()
+            expect(await compContainer.innerText()).toContain('option2', 'clicked')
+        })
+    })
+    test('Options', async ({page}) => {
+        const dropdownSection = page.locator('//select')
+  
+        await test.step('Option 1', async() => {
+            await dropdownSection.selectOption('option1')
+        })     
+        await test.step('Option 1', async() => {
+            await dropdownSection.selectOption('option2')
+        })     
+        await test.step('Option 1', async() => {
+            await dropdownSection.selectOption('option3')
+        })     
+        await test.step('Default option', async() => {
+            await dropdownSection.selectOption('Select an option')
+        })   
     })
 })
