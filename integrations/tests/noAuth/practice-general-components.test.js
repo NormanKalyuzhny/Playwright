7c0c2fbe72255c63989e79b34a0c5758323734e5
NormanKalyuzhny <innerspiritu@gmail.com>
2024-07-01 21:49:06 +0300

    Test added

--- integrations/tests/noAuth/practice-general-components.test.js
+++ integrations/tests/noAuth/practice-general-components.test.js
@@ -1,4 +1,5 @@
 import { test,expect } from "@playwright/test"  
+import { url } from "node:inspector"
 import { before, beforeEach } from "node:test"
 
 test.describe('Basic actions', ()=>{
@@ -6,7 +7,6 @@ test.describe('Basic actions', ()=>{
         await page.goto('https://commitquality.com/practice-general-components')
     })
     test('Buttons', async ({page})=>{
-        
         const btnContainer = page.locator('//div[@class="button-container"]')
         const clickMeBtn = btnContainer.locator('//button[@data-testid="basic-click"]')
         const doubleClickBtn = btnContainer.locator('//button[@data-testid="double-click"]')
@@ -26,33 +26,68 @@ test.describe('Basic actions', ()=>{
         })
     })
     test('Radio buttons', async ({page}) => {
-        const compContainer = page.locator('//div[@class="radio-buttons-container container-outline"]')
-        const radioBtn1 = compContainer.locator('//input[@data-testid="option1"]')
-        const radioBtn2 = compContainer.locator('//input[@data-testid="option2"]')
+        const radioContainer = page.locator('//div[@class="radio-buttons-container container-outline"]')
+        const radioBtn1 = radioContainer.locator('//input[@data-testid="option1"]')
+        const radioBtn2 = radioContainer.locator('//input[@data-testid="option2"]')
 
         await test.step('Radio button 1', async() => {
             await radioBtn1.click()  
-            expect(await compContainer.innerText()).toContain('option1', 'clicked')
+            expect(await radioContainer.innerText()).toContain('option1', 'clicked')
         })
         await test.step('Radio button 2', async() => {
             await radioBtn2.click()
-            expect(await compContainer.innerText()).toContain('option2', 'clicked')
+            expect(await radioContainer.innerText()).toContain('option2', 'clicked')
         })
     })
     test('Options', async ({page}) => {
-        const dropdownSection = page.locator('//select')
+        const dropdownContainer = page.locator('//select')
   
         await test.step('Option 1', async() => {
-            await dropdownSection.selectOption('option1')
+            await dropdownContainer.selectOption('option1')
         })     
         await test.step('Option 1', async() => {
-            await dropdownSection.selectOption('option2')
+            await dropdownContainer.selectOption('option2')
         })     
         await test.step('Option 1', async() => {
-            await dropdownSection.selectOption('option3')
+            await dropdownContainer.selectOption('option3')
         })     
         await test.step('Default option', async() => {
-            await dropdownSection.selectOption('Select an option')
+            await dropdownContainer.selectOption('Select an option')
         })   
     })
+    test('Checkboxes', async ({page}) => {
+        const checkBoxContainer = page.locator('//div[@class="checkbox-container container-outline"]')
+        const checkBox1 = checkBoxContainer.locator('//input[@data-testid="checkbox1"]')
+        const checkBox2 = checkBoxContainer.locator('//input[@data-testid="checkbox2"]')
+        const checkBox3 = checkBoxContainer.locator('//input[@data-testid="checkbox3"]')
+  
+        await test.step('Option 1', async() => {
+            await checkBox1.click()
+            expect(await checkBoxContainer.innerText()).toContain('Checkbox 1 checked')
+        })     
+        await test.step('Option 2', async() => {
+            await checkBox2.click()
+            expect(await checkBoxContainer.innerText()).toContain('Checkbox 2 checked')
+        })     
+        await test.step('Option 3', async() => {
+            await checkBox3.click()
+            expect(await checkBoxContainer.innerText()).toContain('Checkbox 3 checked')
+        })     
+ 
+    })
+    test('Links', async ({page}) => {
+        const linkContainer = page.locator('//div[@class="links-container container-outline"]')
+        const youTube = linkContainer.locator('//a[@data-testid="link-same-tab"]')
+        const youTubeNewTab = linkContainer.locator('//a[@data-testid="link-newtab"]')
+        const practicePage = linkContainer.locator('//a[@data-testid="link-newtab-practice"]')
+
+        await test.step('YouTube', async() => {  
+            await youTube.click()
+            const url = page.url()
+            await page.waitForURL()
+            expect(url).toEqual('https://www.youtube.com/@commitquality')
+        })      
+    })
+    
+
 })
