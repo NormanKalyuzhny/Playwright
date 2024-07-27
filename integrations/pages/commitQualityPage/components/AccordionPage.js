39c3acfce0af93024151d34307f099bf2e0345b8
NormanKalyuzhny <innerspiritu@gmail.com>
2024-07-27 13:06:28 +0300

    test updated

--- integrations/pages/commitQualityPage/components/AccordionPage.js
+++ integrations/pages/commitQualityPage/components/AccordionPage.js
@@ -2,25 +2,25 @@ import { expect } from "@playwright/test";
 
 export default class AccordionPage{
     constructor(page){
-        this.page = page
-        this.mainUrl = 'https://commitquality.com/practice-accordions'
+        this.page = page;
+        this.mainUrl = 'https://commitquality.com/practice-accordions';
         this.btnAccordion1 = this.page.locator('//button[text()="Accordion 1"]');
         this.btnContainer = this.page.locator('//div[@class="button-container"]');
         this.btnClickMe = this.btnContainer.locator('//button[@data-testid="basic-click"]');
         this.btnDbClickMe = this.btnContainer.locator('//button[@data-testid="double-click"]');
         this.btnRightClickMe = this.btnContainer.locator('//button[@data-testid="right-click"]');
         this.btnAccordion2 = this.page.locator('//button[text()="Accordion 2"]');
-        this.radioBtn1 = this.page.locator('//input[@data-testid="option1"]')
-        this.radioBtn2 = this.page.locator('//input[@data-testid="option2"]')
-        this.componentContainer = this.page.locator('//div[@class="component-container"]')
+        this.radioBtn1 = this.page.locator('//input[@data-testid="option1"]');
+        this.radioBtn2 = this.page.locator('//input[@data-testid="option2"]');
+        this.componentContainer = this.page.locator('//div[@class="component-container"]');
         this.btnAccordion3 = this.page.locator('//button[text()="Accordion 3"]');
-        this.checkBox1 = this.page.locator('//input[@data-testid="checkbox1"]')
-        this.checkBox2 = this.page.locator('//input[@data-testid="checkbox2"]')
-        this.checkBox3 = this.page.locator('//input[@data-testid="checkbox3"]')
-        this.checkBoxContainer = this.page.locator('//div[@class="checkbox-container"]')
+        this.checkBox1 = this.page.locator('//input[@data-testid="checkbox1"]');
+        this.checkBox2 = this.page.locator('//input[@data-testid="checkbox2"]');
+        this.checkBox3 = this.page.locator('//input[@data-testid="checkbox3"]');
+        this.checkBoxContainer = this.page.locator('//div[@class="checkbox-container"]');
     }
     mainUrlLogin = async () => {
-        await this.page.goto(this.mainUrl)
+        await this.page.goto(this.mainUrl);
     }
     expRadioBtnText = async (expText) => {
         let checExpText;
@@ -32,11 +32,11 @@ export default class AccordionPage{
                 break;
             }
         }
-        expect(checExpText).toBeTruthy()
+        expect(checExpText).toBeTruthy();
     }
     expCheckBoxText = async (expText) => {
         let checExpText;
-        const elements = await this.checkBoxContainer.all()
+        const elements = await this.checkBoxContainer.all();
         for (const element of elements){
             const text = await element.innerText(); 
             if (text.includes(expText)) {
@@ -44,7 +44,7 @@ export default class AccordionPage{
                 break; 
             }
         }
-        expect(checExpText).toBeTruthy()
+        expect(checExpText).toBeTruthy();
     }
     
 }
--- integrations/tests/noAuth/Accordion.test.js
+++ integrations/tests/noAuth/Accordion.test.js
@@ -2,8 +2,8 @@ import {test,expect} from "@playwright/test";
 import AccordionPage from "../../pages/commitQualityPage/components/AccordionPage";
 test.describe('Accordions tests', ()=>{
     test('Accordion elements', async ({page})=>{
-        const accordionPage = new AccordionPage(page)
-        accordionPage.mainUrlLogin()
+        const accordionPage = new AccordionPage(page);
+        accordionPage.mainUrlLogin();
         await test.step('Buttons', async ()=>{
             await accordionPage.btnAccordion1.click();
             expect(accordionPage.btnContainer).toBeDefined();
@@ -17,21 +17,21 @@ test.describe('Accordions tests', ()=>{
         await test.step('Radio buttons', async ()=>{
             await accordionPage.btnAccordion2.click();
             expect(accordionPage.componentContainer).toBeDefined();
-            await accordionPage.radioBtn1.click()
-            await accordionPage.expRadioBtnText('option1 clicked')
-            await accordionPage.radioBtn2.click()
-            await accordionPage.expRadioBtnText('option2 clicked')
+            await accordionPage.radioBtn1.click();
+            await accordionPage.expRadioBtnText('option1 clicked');
+            await accordionPage.radioBtn2.click();
+            await accordionPage.expRadioBtnText('option2 clicked');
         })
         await test.step('Checkboxes', async () => {
             await accordionPage.btnAccordion3.click();
             expect(accordionPage.checkBoxContainer).toBeDefined();
-            await accordionPage.checkBox1.click()
-            await accordionPage.expCheckBoxText('Checkbox 1 checked')
-            await accordionPage.checkBox2.click()
-            await accordionPage.expCheckBoxText('Checkbox 2 checked')
-            await accordionPage.checkBox3.click()
-            await accordionPage.expCheckBoxText('Checkbox 3 checked')
-            await page.close()
+            await accordionPage.checkBox1.click();
+            await accordionPage.expCheckBoxText('Checkbox 1 checked');
+            await accordionPage.checkBox2.click();
+            await accordionPage.expCheckBoxText('Checkbox 2 checked');
+            await accordionPage.checkBox3.click();
+            await accordionPage.expCheckBoxText('Checkbox 3 checked');
+            await page.close();
         })
     })
 })
